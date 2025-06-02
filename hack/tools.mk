5527f3bac03b987f6c77c4a242242868a8507eed
Plamen Kokanov <plamen.kokanov@sap.com>
2025-06-02 12:16:15 +0300

    Fixes logcheck build when used in another repo

--- hack/tools.mk
+++ hack/tools.mk
@@ -206,7 +206,9 @@ $(LOGCHECK): $(TOOLS_PKG_PATH)/logcheck/go.* $(shell find $(TOOLS_PKG_PATH)/logc
 	cd $(TOOLS_PKG_PATH)/logcheck;GOTOOLCHAIN=auto CGO_ENABLED=1 go build -o $(abspath $(LOGCHECK)) -buildmode=plugin ./plugin
 else
 $(LOGCHECK): go.mod
-	GOTOOLCHAIN=auto CGO_ENABLED=1 go build -o $(LOGCHECK) -buildmode=plugin github.com/gardener/gardener/hack/tools/logcheck/plugin
+# cd to the directory where the github.com/gardener/gardener/hack/tools/logcheck module was downloaded before building,
+# when importing logcheck from a different repo, otherwise the go.mod of that repo's directory is used.
+	cd $(shell go list -tags tools -f '{{ .Dir }}' github.com/gardener/gardener/hack/tools/logcheck 2>/dev/null);GOTOOLCHAIN=auto CGO_ENABLED=1 go build -o $(LOGCHECK) -buildmode=plugin ./plugin
 endif
 
 $(MOCKGEN): $(call tool_version_file,$(MOCKGEN),$(MOCKGEN_VERSION))
