5fdf771c3700c3c0a951d4673f965a17d9cd3404
Plamen Kokanov <plamen.kokanov@sap.com>
2025-09-03 16:10:05 +0300

    Allow overwriting service hostIPs based on seed

--- pkg/provider-local/controller/service/options.go
+++ pkg/provider-local/controller/service/options.go
@@ -5,6 +5,9 @@
 package service
 
 import (
+	"os"
+	"strings"
+
 	"github.com/spf13/pflag"
 
 	"github.com/gardener/gardener/extensions/pkg/controller/cmd"
@@ -16,6 +19,8 @@ type ControllerOptions struct {
 	MaxConcurrentReconciles int
 	// HostIP is the host ip.
 	HostIP string
+	// SeedHostIPOverwrites is a list of seeds and the host ips that should be used for each.
+	SeedHostIPOverwrites []string
 	// Zone0IP is the IP address to be used for the zone 0 istio ingress gateway.
 	Zone0IP string
 	// Zone1IP is the IP address to be used for the zone 1 istio ingress gateway.
@@ -32,6 +37,7 @@ type ControllerOptions struct {
 func (c *ControllerOptions) AddFlags(fs *pflag.FlagSet) {
 	fs.IntVar(&c.MaxConcurrentReconciles, cmd.MaxConcurrentReconcilesFlag, c.MaxConcurrentReconciles, "The maximum number of concurrent reconciliations.")
 	fs.StringVar(&c.HostIP, "host-ip", c.HostIP, "Overwrite Host IP to use for kube-apiserver service LoadBalancer")
+	fs.StringArrayVar(&c.SeedHostIPOverwrites, "seed-host-ip-overwrites", c.SeedHostIPOverwrites, "Overwrite Host IPs to use for kube-apiserver service LoadBalancer depending on the seed. If the name of the seed where provider-local runs does not exist in the specified list, it falls back to host-ip, if it is defined")
 	fs.StringVar(&c.Zone0IP, "zone-0-ip", c.Zone0IP, "Overwrite IP to use for kube-apiserver service LoadBalancer in zone 0")
 	fs.StringVar(&c.Zone1IP, "zone-1-ip", c.Zone1IP, "Overwrite IP to use for kube-apiserver service LoadBalancer in zone 1")
 	fs.StringVar(&c.Zone2IP, "zone-2-ip", c.Zone2IP, "Overwrite IP to use for kube-apiserver service LoadBalancer in zone 2")
@@ -40,7 +46,20 @@ func (c *ControllerOptions) AddFlags(fs *pflag.FlagSet) {
 
 // Complete implements Completer.Complete.
 func (c *ControllerOptions) Complete() error {
-	c.config = &ControllerConfig{c.MaxConcurrentReconciles, c.HostIP, c.Zone0IP, c.Zone1IP, c.Zone2IP, c.BastionIP}
+	hostIP := c.HostIP
+
+	seedName := os.Getenv("SEED_NAME")
+	if len(c.SeedHostIPOverwrites) > 0 {
+		for _, seedHostIP := range c.SeedHostIPOverwrites {
+			// TODO: return err if SeedHostIPOverwrites arg is invalid
+			seed, hostIPForSeed := getSeedAndHostIP(seedHostIP)
+			if seed == seedName {
+				hostIP = hostIPForSeed
+			}
+		}
+	}
+
+	c.config = &ControllerConfig{c.MaxConcurrentReconciles, hostIP, c.Zone0IP, c.Zone1IP, c.Zone2IP, c.BastionIP}
 	return nil
 }
 
@@ -73,4 +92,9 @@ func (c *ControllerConfig) Apply(opts *AddOptions) {
 	opts.Zone1IP = c.Zone1IP
 	opts.Zone2IP = c.Zone2IP
 	opts.BastionIP = c.BastionIP
+}
+
+func getSeedAndHostIP(seedHostIP string) (string, string) {
+	elems := strings.Split(seedHostIP, "=")
+	return elems[0], elems[1]
 }
